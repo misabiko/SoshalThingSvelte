9a6dc6c7a3ddb37abe0c89bd587e24b590ac0d8d
misabiko <misabiko@gmail.com>
2022-03-18 23:26:33 -0400

    global css

--- src/TimelineContainer.svelte
+++ src/TimelineContainer.svelte
@@ -2,6 +2,15 @@
 	import Timeline from './Timeline.svelte';
 </script>
 
+<style lang='sass'>
+	#timelineContainer
+		@include pretty-scrollbar
+		height: 100%
+		overflow-x: auto
+		display: flex
+		flex-grow: 1
+</style>
+
 <div id='timelineContainer'>
 	<Timeline title='Timeline43!'/>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,62 @@
 <script>
 	import TimelineContainer from "../TimelineContainer.svelte";
 
-
 	//chrome.runtime.sendMessage("ialpimkfmdjoekolcmhnajfkmhchkmbd", "message to extension", response => console.dir(response));
 </script>
+
+<style lang="sass" global>
+	@import "../../node_modules/bulma/bulma.sass"
+
+	html
+		overflow-y: hidden
+		//height: -webkit-fill-available*/
+
+	//TODO Fix 100% height on mobile, maybe using -webkit-fill-available
+	body
+		margin: 0
+		height: 100vh
+		//height: -webkit-fill-available
+		-webkit-font-smoothing: antialiased
+		-moz-osx-font-smoothing: grayscale
+		display: flex
+
+	.input.is-static
+		color: $text
+
+	.icon
+		color: $white-ter
+
+	.icon.darkIcon
+		color: $black-ter
+
+	.dropdown-trigger .icon
+		color: $button-color
+
+	.favviewer
+		@import "../../node_modules/bulma/bulma.sass"
+
+		height: 100%
+		display: flex
+
+	#soshal-notifications
+		position: absolute
+		right: 0
+		bottom: 0
+		padding: 15px
+		z-index: 1
+
+	img.emoji
+		height: 1em
+		width: 1em
+		margin: 0 .05em 0 .1em
+		vertical-align: -0.1em
+
+	//#favviewer
+	//	width: 100vw
+	//	height: 90vh
+	//	display: flex
+	//	-webkit-font-smoothing: antialiased
+	//	-moz-osx-font-smoothing: grayscale
+</style>
 
 <TimelineContainer/>

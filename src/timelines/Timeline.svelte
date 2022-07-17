b7cf774177e29ebc74669dd89fc55a5058094f92
misabiko <misabiko@gmail.com>
2022-07-17 17:37:49 -0400

    Moved TimelineOptions to its own component

--- src/timelines/Timeline.svelte
+++ src/timelines/Timeline.svelte
@@ -1,14 +1,8 @@
 <script lang='ts'>
-	import {Field, Input, Select, Switch} from 'svelma'
 	import type {Readable} from 'svelte/store'
 	import {derived, get} from 'svelte/store'
-	import type {ArticleIdPair, ArticleRef, ArticleWithRefs} from '../services/article'
+	import type {ArticleRef, ArticleWithRefs} from '../services/article'
 	import Article, {articleRefIdPairToRef} from '../services/article'
-	import ColumnContainer from "../containers/ColumnContainer.svelte"
-	import RowContainer from "../containers/RowContainer.svelte"
-	import MasonryContainer from "../containers/MasonryContainer.svelte"
-	import SocialArticleView from "../articles/SocialArticleView.svelte"
-	import GalleryArticleView from "../articles/GalleryArticleView.svelte"
 	import {
 		getEndpoints,
 		getWritable,
@@ -18,11 +12,10 @@
 	import {onMount} from 'svelte'
 	import {type TimelineData} from './index'
 	import {keepArticle} from '../filters'
-	import FiltersOptions from "../filters/FiltersOptions.svelte";
 	import {compare, SortMethod} from '../sorting'
-	import SortOptions from "../sorting/SortOptions.svelte";
 	import type {ContainerProps} from '../containers'
 	import TimelineHeader from "./TimelineHeader.svelte";
+	import TimelineOptions from "./TimelineOptions.svelte";
 
 	export let data: TimelineData
 	export let fullscreen: boolean
@@ -197,26 +190,6 @@
 		width: unset
 		max-width: 100%
 
-	.timelineOptions
-		background-color: $scheme-main-ter
-		padding: 1rem
-		display: flex
-		flex-direction: column
-		align-items: flex-start
-		overflow-x: hidden
-		overflow-y: scroll
-		min-height: 50%
-
-		//TODO & input[type="number"]
-		//	width: 200px
-
-		& > .box
-			max-width: 100%
-			width: 100%
-
-	:global(#timelineContainer .timelineOptions::-webkit-scrollbar-thumb)
-		background-color: $dark
-
 	:global(.articlesContainer)
 		overflow-y: scroll
 		overflow-x: hidden
@@ -240,67 +213,11 @@
 		on:refresh={e => refresh(e.detail)}
 	/>
 	{#if showOptions}
-		<div class='timelineOptions'>
-			<div class='box'>
-				<Field label='Container'>
-					<Select bind:selected={data.container} nativeSize={0}>
-						<option value={ColumnContainer}>Column</option>
-						<option value={RowContainer}>Row</option>
-						<option value={MasonryContainer}>Masonry</option>
-					</Select>
-				</Field>
-				{#if data.container !== ColumnContainer}
-					<Field label='Column Count'>
-						<Input type='number' bind:value={data.columnCount} min={1}/>
-					</Field>
-				{/if}
-				{#if !fullscreen}
-					<Field label='Timeline Width'>
-						<Input type='number' bind:value={data.width} min={1}/>
-					</Field>
-				{/if}
-				<Field label='AutoScroll Speed'>
-					<Input type='number' bind:value={data.scrollSpeed} min={0}/>
-				</Field>
-			</div>
-			<div class='box'>
-				<Field label='Article View'>
-					<Select bind:selected={data.articleView} nativeSize={0}>
-						<option value={SocialArticleView}>Social</option>
-						<option value={GalleryArticleView}>Gallery</option>
-					</Select>
-				</Field>
-				<div class='field'>
-					<Switch bind:checked={data.animatedAsGifs}>Show all animated as gifs</Switch>
-				</div>
-<!--				<div class='field'>-->
-<!--					<Switch bind:checked={lazyLoading}>Lazy media loading</Switch>-->
-<!--				</div>-->
-				{#if data.articleView === SocialArticleView}
-					<div class='field'>
-						<Switch bind:checked={data.compact}>Compact articles</Switch>
-					</div>
-					<div class='field'>
-						<Switch bind:checked={data.hideText}>Hide text</Switch>
-					</div>
-				{/if}
-			</div>
-			<div class='box'>
-				<Field label='Endpoints'>
-					<ul>
-						{#each data.endpoints as endpoint (endpoint)}
-							<li>{endpoint.name}</li>
-						{/each}
-					</ul>
-				</Field>
-			</div>
-			<div class='box'>
-				<FiltersOptions bind:instances={data.filters}/>
-			</div>
-			<div class='box'>
-				<SortOptions bind:sortInfo={data.sortInfo} on:sortOnce={sortOnce}/>
-			</div>
-		</div>
+		<TimelineOptions
+			bind:data
+			bind:fullscreen
+			on:sortOnce
+		/>
 	{/if}
 	<svelte:component
 		this={data.container}
---
+++ src/timelines/TimelineOptions.svelte
@@ -0,0 +1,103 @@
+<script lang='ts'>
+	import {Field, Input, Select, Switch} from 'svelma'
+	import ColumnContainer from "../containers/ColumnContainer.svelte"
+	import RowContainer from "../containers/RowContainer.svelte"
+	import MasonryContainer from "../containers/MasonryContainer.svelte"
+	import SocialArticleView from "../articles/SocialArticleView.svelte"
+	import GalleryArticleView from "../articles/GalleryArticleView.svelte"
+	import type {TimelineData} from './index'
+	import FiltersOptions from "../filters/FiltersOptions.svelte"
+	import SortOptions from "../sorting/SortOptions.svelte"
+	import {createEventDispatcher} from 'svelte'
+
+	export let data: TimelineData
+	export let fullscreen: boolean
+
+	const dispatch = createEventDispatcher()
+</script>
+
+<style lang='sass'>
+	@use '../styles/variables' as *
+
+	.timelineOptions
+		background-color: $scheme-main-ter
+		padding: 1rem
+		display: flex
+		flex-direction: column
+		align-items: flex-start
+		overflow-x: hidden
+		overflow-y: scroll
+		min-height: 50%
+
+		//TODO & input[type="number"]
+		//	width: 200px
+
+		& > .box
+			max-width: 100%
+			width: 100%
+
+	:global(#timelineContainer .timelineOptions::-webkit-scrollbar-thumb)
+		background-color: $dark
+</style>
+
+<div class='timelineOptions'>
+	<div class='box'>
+		<Field label='Container'>
+			<Select bind:selected={data.container} nativeSize={0}>
+				<option value={ColumnContainer}>Column</option>
+				<option value={RowContainer}>Row</option>
+				<option value={MasonryContainer}>Masonry</option>
+			</Select>
+		</Field>
+		{#if data.container !== ColumnContainer}
+			<Field label='Column Count'>
+				<Input type='number' bind:value={data.columnCount} min={1}/>
+			</Field>
+		{/if}
+		{#if !fullscreen}
+			<Field label='Timeline Width'>
+				<Input type='number' bind:value={data.width} min={1}/>
+			</Field>
+		{/if}
+		<Field label='AutoScroll Speed'>
+			<Input type='number' bind:value={data.scrollSpeed} min={0}/>
+		</Field>
+	</div>
+	<div class='box'>
+		<Field label='Article View'>
+			<Select bind:selected={data.articleView} nativeSize={0}>
+				<option value={SocialArticleView}>Social</option>
+				<option value={GalleryArticleView}>Gallery</option>
+			</Select>
+		</Field>
+		<div class='field'>
+			<Switch bind:checked={data.animatedAsGifs}>Show all animated as gifs</Switch>
+		</div>
+		<!--				<div class='field'>-->
+		<!--					<Switch bind:checked={lazyLoading}>Lazy media loading</Switch>-->
+		<!--				</div>-->
+		{#if data.articleView === SocialArticleView}
+			<div class='field'>
+				<Switch bind:checked={data.compact}>Compact articles</Switch>
+			</div>
+			<div class='field'>
+				<Switch bind:checked={data.hideText}>Hide text</Switch>
+			</div>
+		{/if}
+	</div>
+	<div class='box'>
+		<Field label='Endpoints'>
+			<ul>
+				{#each data.endpoints as endpoint (endpoint)}
+					<li>{endpoint.name}</li>
+				{/each}
+			</ul>
+		</Field>
+	</div>
+	<div class='box'>
+		<FiltersOptions bind:instances={data.filters}/>
+	</div>
+	<div class='box'>
+		<SortOptions bind:sortInfo={data.sortInfo} on:sortOnce={dispatch('sortOnce')}/>
+	</div>
+</div>

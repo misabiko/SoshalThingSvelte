cce041e964ac2fa99969ceee9fb7118bf63547a2
misabiko <misabiko@gmail.com>
2022-07-17 18:28:30 -0400

    Dissolved index.svelte into entry.ts

--- src/entry.ts
+++ src/entry.ts
@@ -1,5 +1,19 @@
-import Root from './index.svelte';
+import 'styles/global.sass'
 import './services/twitter/endpoints'
 import './services/dummy/endpoints'
+import SoshalThing from "./SoshalThing.svelte"
+import {loadMainStorage, loadTimelines} from './storages'
 
-new Root({ target: document.body });
+const {fullscreen} = loadMainStorage();
+const initTimelines = loadTimelines();
+
+//TODO scroll wrong direction
+
+new SoshalThing({
+	target: document.body,
+	props: {
+		isInjected: false,
+		initTimelines,
+		fullscreen,
+	}
+})

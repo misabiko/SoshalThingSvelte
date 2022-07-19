f215042162abeb31892d9c789891eb36301ef8bb
misabiko <misabiko@gmail.com>
2022-07-18 20:27:07 -0400

    Moved social nav to SocialNav

--- src/articles/social/SocialArticleView.svelte
+++ src/articles/social/SocialArticleView.svelte
@@ -1,20 +1,10 @@
 <script lang='ts'>
-	import Fa from 'svelte-fa/src/fa.svelte'
-	import {faHeart} from '@fortawesome/free-regular-svg-icons'
-	import {
-		faRetweet,
-		faHeart as faHeartFilled,
-		faEyeSlash,
-		faEllipsisH, faExpandAlt, faEye,
-	} from '@fortawesome/free-solid-svg-icons'
 	import {createEventDispatcher} from 'svelte'
 	import Article from '../../services/article'
-	import Dropdown from '../../Dropdown.svelte'
-	import {toggleMarkAsRead, toggleHide, articleAction, getArticleAction, STANDARD_ACTIONS} from "../../services/service"
-	import {MediaType} from "../../services/article.js";
 	import type {ArticleProps, TimelineArticleProps} from '../index'
 	import {shortTimestamp} from "../index";
 	import SocialMedia from "./SocialMedia.svelte";
+	import SocialNav from "./SocialNav.svelte";
 
 	export let timelineProps: TimelineArticleProps
 	export let articleProps: ArticleProps
@@ -95,81 +85,6 @@
 	p.articleParagraph
 		white-space: pre-line
 		overflow-wrap: anywhere
-
-
-	.articleButton
-		color: $light
-		font-size: unset
-
-		&:focus
-			outline: none
-
-		&:hover span
-			color: $primary
-
-		//&:hover.commentButton span
-		//	color: $comment-color
-
-	button:disabled
-		cursor: unset
-
-	//.fade-enter-active, .fade-leave-active
-	//	transition: opacity .5s
-	//
-	//.fade-enter, .fade-leave-to
-	//	opacity: 0
-
-	button.likeButton
-		color: $light
-
-		&:hover, &.likedPostButton
-			span, :global(span > svg)
-				color: $like-color
-
-	//@keyframes heart
-	//	0%, 17.5%
-	//		width: 0
-	//
-	//.heart-enter-active
-	//	will-change: width
-	//	animation: heart .5s cubic-bezier(.17, .89, .32, 1.49)
-	//
-	//.heart-enter
-	//	width: 0
-	//
-	//$bubble-d: 2em
-	//$bubble-r: .5 * $bubble-d
-	//
-	//.icon > svg
-	//	position: relative
-	//
-	//	&:before, &:after
-	//		position: absolute
-	//		z-index: -1
-	//		top: 50%
-	//		left: 50%
-	//		border-radius: 50%
-	//		content: ''
-	//
-	//	&::before
-	//		margin: -$bubble-r
-	//		width: $bubble-d
-	//		height: $bubble-d
-	//		background: gold
-
-	button.repostButton
-		color: $light
-
-		&:hover, &.repostedPostButton
-			span, :global(span > svg)
-				color: $repost-color
-
-	//:global(article.socialArticle .icon > svg)
-	//	will-change: transform
-	//	transition: transform .5s ease-in-out
-
-	//:global(.repostedPostButton .icon > svg)
-	//	transform: rotate(360deg)
 
 	//, .replyLabel
 	.repostLabel
@@ -281,7 +196,7 @@
 							</small>
 						</span>
 					</div>
-					{#if !minimized}
+					{#if !(minimized || quoted.markedAsRead || quoted.hidden)} <!--	TODO Filter from timeline-->
 						{#if !timelineProps.hideText}
 							<p class='refArticleParagraph'>
 								{#if quoted.textHtml}
@@ -297,102 +212,22 @@
 							on:mediaClick={e => dispatch('mediaClick', e.detail)}
 						/>
 					{/if}
-					<!--TODO SocialNav-->
+					<SocialNav
+						article={quoted}
+						isQuoted={true}
+						{timelineProps}
+						{modal}
+					/>
 				</div>
 			{/if}
-			<nav class='level is-mobile'>
-				<div class='level-left'>
-					{#if getArticleAction(STANDARD_ACTIONS.repost, actualArticle.idPair.service)}
-						<button
-							class='level-item articleButton repostButton borderless-button'
-							class:repostedPostButton={actualArticle.getReposted()}
-							title='Repost'
-							on:click={() => articleAction(STANDARD_ACTIONS.repost, actualArticle.idPair)}
-							disabled={actualArticle.getReposted() && !getArticleAction(STANDARD_ACTIONS.repost, actualArticle.idPair.service).togglable}
-						>
-							<span class='icon'>
-								<Fa icon={faRetweet}/>
-							</span>
-							{#if actualArticle.getRepostCount()}
-								<span>{actualArticle.getRepostCount()}</span>
-							{/if}
-						</button>
-					{/if}
-					{#if getArticleAction(STANDARD_ACTIONS.like, actualArticle.idPair.service)}
-						<button
-							class='level-item articleButton likeButton borderless-button'
-							class:likedPostButton={actualArticle.getLiked()}
-							title='Like'
-							on:click={() => articleAction(STANDARD_ACTIONS.like, actualArticle.idPair)}
-							disabled={actualArticle.getLiked() && !getArticleAction(STANDARD_ACTIONS.like, actualArticle.idPair.service).togglable}
-						>
-							<span class='icon'>
-								<Fa icon={actualArticle.getLiked() ? faHeartFilled : faHeart}/>
-							</span>
-							{#if actualArticle.getLikeCount()}
-								<span>{actualArticle.getLikeCount()}</span>
-							{/if}
-						</button>
-					{/if}
-					<button
-						class='level-item articleButton borderless-button'
-						title='Mark as read'
-						on:click={() => toggleMarkAsRead(actualArticle.idPair)}
-					>
-						<span class='icon'>
-							<Fa icon={actualArticle.markedAsRead ? faEye : faEyeSlash}/>
-						</span>
-					</button>
-					{#if !modal}
-						<button
-							class='level-item articleButton borderless-button'
-							title='Expand article as modal'
-							on:click={() => modal = true}
-						>
-							<span class='icon'>
-								<Fa icon={faExpandAlt}/>
-							</span>
-						</button>
-					{/if}
-					<Dropdown labelClasses='articleButton borderless-button'>
-						<span slot='triggerIcon' class='icon'>
-							<Fa icon={faEllipsisH} class='level-item'/>
-						</span>
-
-						<!-- svelte-ignore a11y-missing-attribute -->
-						<a class='dropdown-item' on:click={() => toggleMarkAsRead(actualArticle.idPair)}>
-							Mark as read
-						</a>
-						<!-- svelte-ignore a11y-missing-attribute -->
-						<a class='dropdown-item' on:click={() => toggleHide(actualArticle.idPair)}>
-							Hide
-						</a>
-						<!-- svelte-ignore a11y-missing-attribute -->
-						<a class='dropdown-item' on:click={() => timelineProps.compact = !timelineProps.compact}>
-							{ timelineProps.compact ? 'Show expanded' : 'Show compact' }
-						</a>
-						<a class='dropdown-item' href={ actualArticle.url } target='_blank'>
-							External Link
-						</a>
-						{#if isArticleRepost}
-							<a class='dropdown-item' href={ articleProps.article.url } target='_blank'>
-								Repost's external Link
-							</a>
-						{/if}
-						<!-- svelte-ignore a11y-missing-attribute -->
-						<a class='dropdown-item' on:click={() => dispatch('logData')}>
-							Log Data
-						</a>
-						<!-- svelte-ignore a11y-missing-attribute -->
-						<a class='dropdown-item' on:click={() => dispatch('logJSON')}>
-							Log Json Data
-						</a>
-						<!--	<a class='dropdown-item'>-->
-						<!--		Fetch Data-->
-						<!--	</a>-->
-					</Dropdown>
-				</div>
-			</nav>
+			<SocialNav
+				article={actualArticle}
+				bind:modal
+				{timelineProps}
+				repost={isArticleRepost ? articleProps.article : undefined}
+				on:logData={() => dispatch('logData')}
+				on:logJSON={() => dispatch('logJSON')}
+			/>
 		</div>
 	</div>
 	{#if actualArticle.medias.length && !minimized}
---
+++ src/articles/social/SocialNav.svelte
@@ -0,0 +1,197 @@
+<script lang='ts'>
+	import Fa from 'svelte-fa/src/fa.svelte'
+	import {faHeart} from '@fortawesome/free-regular-svg-icons'
+	import {
+		faRetweet,
+		faHeart as faHeartFilled,
+		faEyeSlash,
+		faEllipsisH, faExpandAlt, faEye,
+	} from '@fortawesome/free-solid-svg-icons'
+	import Dropdown from '../../Dropdown.svelte'
+	import {toggleMarkAsRead, toggleHide, articleAction, getArticleAction, STANDARD_ACTIONS} from "../../services/service"
+	import Article from '../../services/article'
+	import type {TimelineArticleProps} from '../index'
+	import {createEventDispatcher} from 'svelte'
+
+	export let article: Article
+	export let repost: Article | undefined = undefined
+	export let isQuoted = false
+	export let modal: boolean
+	export let timelineProps: TimelineArticleProps
+
+	const dispatch = createEventDispatcher()
+</script>
+
+<style lang='sass'>
+	@use '../../styles/variables' as *
+
+	.articleButton
+		color: $light
+		font-size: unset
+
+		&:focus
+			outline: none
+
+		&:hover span
+			color: $primary
+
+	//&:hover.commentButton span
+	//	color: $comment-color
+
+	button:disabled
+		cursor: unset
+
+	//.fade-enter-active, .fade-leave-active
+	//	transition: opacity .5s
+	//
+	//.fade-enter, .fade-leave-to
+	//	opacity: 0
+
+	button.likeButton
+		color: $light
+
+		&:hover, &.likedPostButton
+			span, :global(span > svg)
+				color: $like-color
+
+	//@keyframes heart
+	//	0%, 17.5%
+	//		width: 0
+	//
+	//.heart-enter-active
+	//	will-change: width
+	//	animation: heart .5s cubic-bezier(.17, .89, .32, 1.49)
+	//
+	//.heart-enter
+	//	width: 0
+	//
+	//$bubble-d: 2em
+	//$bubble-r: .5 * $bubble-d
+	//
+	//.icon > svg
+	//	position: relative
+	//
+	//	&:before, &:after
+	//		position: absolute
+	//		z-index: -1
+	//		top: 50%
+	//		left: 50%
+	//		border-radius: 50%
+	//		content: ''
+	//
+	//	&::before
+	//		margin: -$bubble-r
+	//		width: $bubble-d
+	//		height: $bubble-d
+	//		background: gold
+
+	button.repostButton
+		color: $light
+
+		&:hover, &.repostedPostButton
+			span, :global(span > svg)
+				color: $repost-color
+
+	//:global(article.socialArticle .icon > svg)
+	//	will-change: transform
+	//	transition: transform .5s ease-in-out
+
+	//:global(.repostedPostButton .icon > svg)
+	//	transform: rotate(360deg)
+</style>
+
+<nav class='level is-mobile'>
+	<div class='level-left'>
+		{#if getArticleAction(STANDARD_ACTIONS.repost, article.idPair.service)}
+			<button
+				class='level-item articleButton repostButton borderless-button'
+				class:repostedPostButton={article.getReposted()}
+				title='Repost'
+				on:click={() => articleAction(STANDARD_ACTIONS.repost, article.idPair)}
+				disabled={article.getReposted() && !getArticleAction(STANDARD_ACTIONS.repost, article.idPair.service).togglable}
+			>
+							<span class='icon'>
+								<Fa icon={faRetweet}/>
+							</span>
+				{#if article.getRepostCount()}
+					<span>{article.getRepostCount()}</span>
+				{/if}
+			</button>
+		{/if}
+		{#if getArticleAction(STANDARD_ACTIONS.like, article.idPair.service)}
+			<button
+				class='level-item articleButton likeButton borderless-button'
+				class:likedPostButton={article.getLiked()}
+				title='Like'
+				on:click={() => articleAction(STANDARD_ACTIONS.like, article.idPair)}
+				disabled={article.getLiked() && !getArticleAction(STANDARD_ACTIONS.like, article.idPair.service).togglable}
+			>
+				<span class='icon'>
+					<Fa icon={article.getLiked() ? faHeartFilled : faHeart}/>
+				</span>
+				{#if article.getLikeCount()}
+					<span>{article.getLikeCount()}</span>
+				{/if}
+			</button>
+		{/if}
+		<button
+			class='level-item articleButton borderless-button'
+			title='Mark as read'
+			on:click={() => toggleMarkAsRead(article.idPair)}
+		>
+			<span class='icon'>
+				<Fa icon={article.markedAsRead ? faEye : faEyeSlash}/>
+			</span>
+		</button>
+		{#if !isQuoted && !modal}
+			<button
+				class='level-item articleButton borderless-button'
+				title='Expand article as modal'
+				on:click={() => modal = true}
+			>
+				<span class='icon'>
+					<Fa icon={faExpandAlt}/>
+				</span>
+			</button>
+		{/if}
+		<Dropdown labelClasses='articleButton borderless-button'>
+						<span slot='triggerIcon' class='icon'>
+							<Fa icon={faEllipsisH} class='level-item'/>
+						</span>
+
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a class='dropdown-item' on:click={() => toggleMarkAsRead(article.idPair)}>
+				Mark as read
+			</a>
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a class='dropdown-item' on:click={() => toggleHide(article.idPair)}>
+				Hide
+			</a>
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a class='dropdown-item' on:click={() => timelineProps.compact = !timelineProps.compact}>
+				{ timelineProps.compact ? 'Show expanded' : 'Show compact' }
+			</a>
+			<a class='dropdown-item' href={ article.url } target='_blank'>
+				External Link
+			</a>
+			{#if repost}
+				<a class='dropdown-item' href={ repost.url } target='_blank'>
+					Repost's external Link
+				</a>
+			{/if}
+			{#if !isQuoted}
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<a class='dropdown-item' on:click={() => dispatch('logData')}>
+					Log Data
+				</a>
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<a class='dropdown-item' on:click={() => dispatch('logJSON')}>
+					Log Json Data
+				</a>
+				<!--	<a class='dropdown-item'>-->
+				<!--		Fetch Data-->
+				<!--	</a>-->
+			{/if}
+		</Dropdown>
+	</div>
+</nav>

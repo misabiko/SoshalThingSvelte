301628b41c0b0ff9de50d5717c339fc94f228a33
misabiko <misabiko@gmail.com>
2022-07-26 18:29:42 -0400

    Made multiple attempts to inject

--- src/extension/pixiv/userPage/entry.ts
+++ src/extension/pixiv/userPage/entry.ts
@@ -2,17 +2,22 @@ import UserPage from './UserArtworksPage.svelte';
 
 const path = window.location.pathname.split('/')
 if (path.length === 4 || path[4] === 'illustrations' || path[4] === 'artworks') {
-	console.log('Starting 3000ms timeout...')
-	setTimeout(() => {
-		console.log('Injecting soshalthing...')
-		const ul = document.querySelector('section > div > div > ul')
-		const target = ul?.parentElement?.parentElement?.parentElement
-		if (!target)
-			throw new Error("Couldn't find section")
+	let attempts = 0
+	let ul = null
+	const intervalId = setInterval(() => {
+		console.log('Check if we can inject...')
+		ul = document.querySelector('section > div > div > ul')
+		if (ul !== null) {
+			clearInterval(intervalId)
+			console.log('Injecting soshalthing...')
+			const target = ul?.parentElement?.parentElement?.parentElement
+			if (!target)
+				throw new Error("Couldn't find section")
 
-		new UserPage({
-			target,
-			anchor: ul.parentElement.parentElement,
-		})
-	}, 3000)
+			new UserPage({
+				target,
+				anchor: ul?.parentElement?.parentElement as HTMLElement,
+			})
+		}
+	}, 500)
 }

d2a2f744e5836cc3821cc9e1af2c5d88d277080f
misabiko <misabiko@gmail.com>
2022-08-02 15:13:04 -0400

    Added test for missing service filter field

--- tests/filters.spec.ts
+++ tests/filters.spec.ts
@@ -1,7 +1,12 @@
 import {expect, test} from '@playwright/test'
 import {loadWithLocalStorage, TIMELINE_STORAGE_KEY} from './storages.spec.js'
 
-test.beforeEach(async ({page}) => {
+test('missing filter service', async ({page}) => {
+	page.on('console', message => {
+		expect(message.type()).toBe('error');
+		expect(message.text()).toContain('Missing service');
+	});
+
 	await loadWithLocalStorage(page, {
 		[TIMELINE_STORAGE_KEY]: [{
 			endpoints: [
@@ -10,41 +15,61 @@ test.beforeEach(async ({page}) => {
 					endpointType: 0,
 				}
 			],
-			filters: [
-				{
-					filter: {type: "notMarkedAsRead"},
-					enabled: true,
-					inverted: false,
-				}, {
-					filter: {type: "notHidden"},
-					enabled: true,
-					inverted: false,
-				},
-			]
+			filters: [{
+				filter: {type: 'notMarkedAsRead'},
+				enabled: true,
+				inverted: false,
+			}]
 		}]
 	})
 })
 
-test('mark as read', async ({page}) => {
-	const articleLocator = page.locator('article')
-	const articleCount = await articleLocator.count()
-	expect(articleCount).toBeGreaterThan(0)
+test.describe('mark as read/hide', () => {
+	test.beforeEach(async ({page}) => {
+		await loadWithLocalStorage(page, {
+			[TIMELINE_STORAGE_KEY]: [{
+				endpoints: [
+					{
+						service: 'Dummy',
+						endpointType: 0,
+					}
+				],
+				filters: [
+					{
+						filter: {type: "notMarkedAsRead", service: null},
+						enabled: true,
+						inverted: false,
+					}, {
+						filter: {type: "notHidden", service: null},
+						enabled: true,
+						inverted: false,
+					},
+				]
+			}]
+		})
+	})
 
-	await page.locator('article button.articleButton[title = "Mark as read"]').first().click();
+	test('mark as read', async ({page}) => {
+		const articleLocator = page.locator('article')
+		const articleCount = await articleLocator.count()
+		expect(articleCount).toBeGreaterThan(0)
 
-	await expect(articleLocator).toHaveCount(articleCount - 1);
-})
+		await page.locator('article button.articleButton[title = "Mark as read"]').first().click();
 
-test('hidden', async ({page}) => {
-	const articleLocator = page.locator('article')
-	const articleCount = await articleLocator.count()
-	expect(articleCount).toBeGreaterThan(0)
+		await expect(articleLocator).toHaveCount(articleCount - 1);
+	})
 
-	await page.locator('article .dropdown-trigger button.articleButton').first().click();
+	test('hidden', async ({page}) => {
+		const articleLocator = page.locator('article')
+		const articleCount = await articleLocator.count()
+		expect(articleCount).toBeGreaterThan(0)
 
-	await page.locator('article a.dropdown-item >> text=Hide').first().click();
+		await page.locator('article .dropdown-trigger button.articleButton').first().click();
 
-	await expect(articleLocator).toHaveCount(articleCount - 1);
+		await page.locator('article a.dropdown-item >> text=Hide').first().click();
+
+		await expect(articleLocator).toHaveCount(articleCount - 1);
+	})
 })
 
 test.skip('repost by username', async () => {})
